68e7cfc488d26cae1aa6a64c3ec280c8ca85e818
SabaOkhanashvili <117926789+SabaOkhanashvili@users.noreply.github.com>
2023-03-19 15:45:44 +0400

    Add files via upload

---
+++ practical tasks 2/full in one/script.js
@@ -0,0 +1,22 @@
+
+const modal = document.getElementById('modal');
+const backdrop = document.getElementById('backdrop');
+
+const modalBtn = document.getElementById('modal-btn');
+
+
+const colorBtn = document.getElementById('change-color-btn');
+
+
+const colorInput = document.getElementById('color-input');
+
+
+const closeBtn = document.getElementsByClassName('close')[0];
+
+
+const numbersInput = document.getElementById('numbers-input');
+
+const averageBtn = document.getElementById('average-btn');
+
+
+const averageResult = document
